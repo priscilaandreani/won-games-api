1f59be69bacb3786364f969e3881c7f701c70b32
priscilaandreani <priscilaandreani@gmail.com>
2024-07-29 19:21:49 -0300

    feat: customize icon and logo

---
+++ src/admin/app.tsx
@@ -0,0 +1,18 @@
+import Icon from "./extensions/icon.png";
+import Logo from "./extensions/logo.svg";
+
+export default {
+  config: {
+    head: {
+      favicon: Icon,
+    },
+    auth: {
+      logo: Logo,
+    },
+    menu: {
+      logo: Icon,
+    },
+    locales: [],
+  },
+  bootstrap() {},
+};
