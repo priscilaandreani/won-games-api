6b5ef65b0517a1a8b32f94f2aff06bc9e50b6932
priscilaandreani <priscilaandreani@gmail.com>
2024-08-05 16:48:49 -0300

    feat: customize colors and texts

--- src/admin/app.tsx
+++ src/admin/app.tsx
@@ -12,6 +12,25 @@ export default {
     menu: {
       logo: Icon,
     },
+    translations: {
+      en: {
+        "Auth.form.welcome.title": "Welcome to Won Games",
+        "Auth.form.welcome.subtitle": "Login to your favorite games store",
+        "app.components.LeftMenu.navbrand.title": "Dashboard",
+      },
+    },
+    theme: {
+      light: {},
+      dark: {
+        colors: {
+          primary100: "#030415",
+          primary600: "#f231a5",
+          primary700: "#f231a5",
+          neutral: "#0d102f",
+          neutral100: "#030415",
+        },
+      },
+    },
     locales: [],
   },
   bootstrap() {},
